e670a4a096e75f4272c7c5e1b8e792dc795e6148
AndreiAstalus <30600820+AndreiAstalus@users.noreply.github.com>
2019-10-01 20:09:07 +0300

    Add files via upload

--- MysqlConfiguration.php
+++ MysqlConfiguration.php
@@ -6,23 +6,23 @@ class MysqlConfiguration
 
     public function __construct()
     {
-         $servername = "localhost";
-         $username = "root";
-         $password = "";
-         $dbname = "db";
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "db";
         $this->connection = mysqli_connect($servername, $username, $password, $dbname);
-
     }
 
-    public function getConnection(){
+    public function getConnection()
+    {
         return $this->connection;
     }
 
-    public function CheckConnection(){
-        if ($this->$connection) {
+    public function CheckConnection()
+    {
+        if (!$this->connection) {
             die("Connection failed: " . mysqli_connect_error());
-        }
-        else {
+        } else {
             echo "Connected successfully";
         }
     }
